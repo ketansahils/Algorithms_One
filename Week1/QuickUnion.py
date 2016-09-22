07eb7ee2c361161882821d306e0b68acaa7768e8
ketan <ketansahils@gmail.com>
2016-09-22 23:55:27 +0530

    Add quickunion algorithm with node compression

--- Week1/QuickUnion.py
+++ Week1/QuickUnion.py
@@ -7,11 +7,18 @@ class QuickUnion(object):
 		if self.__array[i] == i: return i
 		return self.root(self.__array[i])
 
+	def rootFlatten(self, p):
+		while self.__array[p] != p:
+			temp = p
+			p = self.__array[p]
+			self.__array[temp] = self.root(p)
+		return p
+
 	def connected(self,i,j):
 		return self.root(i) == self.root(j)
 
 	def union(self,i,j):
-		k = self.root(i)
+		k = self.rootFlatten(i)
 		self.__array[k] = self.root(j)
 
 	def connComps(self):
@@ -26,16 +33,16 @@ class QuickUnion(object):
 if __name__ == '__main__':
 	k = QuickUnion(10)
 	k.union(6,5)
-	k.union(5,0)
+	k.union(7,5)
 	print "0 --- 1 ",k.connected(0,1)
-	k.union(0,1)
-	k.union(2,1)
-	k.union(7,1)
-	k.union(1,8)
-	k.union(4,3)
+	k.union(8,5)
+	k.union(3,4)
+	k.union(2,4)
+	k.union(0,4)
+	k.union(3,5)
 	print "0 --- 3 ",k.connected(0,3)
-	k.union(3,8)
-	k.union(9,8)
+	k.union(3,1)
+	k.union(3,9)
 
 	print "7 --- 6 ",k.connected(7,6)
 	print "9 --- 3 ",k.connected(9,3)
@@ -44,9 +51,9 @@ if __name__ == '__main__':
 	print k.connComps()
 
 	# 0 --- 1  False
-	# 0 --- 3  False
+	# 0 --- 3  True
 	# 7 --- 6  True
 	# 9 --- 3  True
 	# 0  1  2  3  4  5  6  7  8  9  
-	# 1  8  1  8  3  0  5  1  8  8
+	# 4  9  4  1  5  1  5  5  5  9
 	# 1

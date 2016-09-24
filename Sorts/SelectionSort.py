d02af5d63f78f94c581f152d12a59c9aa0911243
ketan <ketansahils@gmail.com>
2016-09-24 18:36:16 +0530

    Add sorts directory Add selectionsort

---
+++ Sorts/SelectionSort.py
@@ -0,0 +1,19 @@
+class Selection(object):
+	def __init__(self,array):
+		self._array = array
+
+	def sort(self):
+		N = len(self._array)
+		for k in range(N):
+			minx = k
+			for i in range(k+1,N):
+				if self._array[i] < self._array[minx]:
+					minx = i
+
+			self._array[k], self._array[minx] = self._array[minx], self._array[k]
+		return self._array
+
+if __name__ == '__main__':
+	arr = [3,4,5,3,3,-2,2,33,5,6,7,8,-8,55]
+	k = Selection(arr)
+	print k.sort()

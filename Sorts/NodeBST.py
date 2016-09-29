92cd15956804a8277e4141097aa9250a85a80d03
ketan <ketansahils@gmail.com>
2016-09-29 23:43:48 +0530

    Add init files and BSTNode DS

---
+++ Sorts/NodeBST.py
@@ -0,0 +1,19 @@
+import os, sys
+sys.path.insert(0, os.path.abspath('..'))
+from WeekX.Node import Node
+
+class NodeBST(Node):
+	def __init__(self,key,val):
+		Node.__init__(self,val,None)
+		del self._next
+		self._left = None
+		self._right = None
+		self._key = key
+
+
+if __name__ == '__main__':
+	k = NodeBST('ketan',424)
+	print k._left
+	print k._right
+	print k._key
+	print k._val

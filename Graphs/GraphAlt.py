0a5f02fa4d8cf60127a035fc046eb821ad668bfa
ketan <ketansahils@gmail.com>
2016-10-10 15:12:28 +0530

    Add DFS Paths, vertex paths, added size to stack API

--- Graphs/GraphAlt.py
+++ Graphs/GraphAlt.py
@@ -1,6 +1,7 @@
 import os, sys
 sys.path.insert(0, os.path.abspath('..'))
 from WeekX.LinkedList import LinkedList
+from WeekX.Stack import Stack
 
 class Bag(LinkedList):
 	def __init__(self,val):
@@ -50,22 +51,47 @@ class Paths(object):
 
 
 class DepthFirstPaths(object):
-	def __init__(self,graph):
+	def __init__(self,graph,source):
 		self._graph = graph
 		self._visited = [False] * self._graph._V
 		self._edge = [_ for _ in range(self._graph._V)]
+		self._source = source
+		self._trodden = False
 
-	def dfs(self,vertex):
+	def _dfs(self,vertex):
 		self._visited[vertex] = True
 		for v in self._graph.adjVertices(vertex):
 			if not self._visited[v]:
-				self.dfs(v)
+				self._dfs(v)
 				self._edge[v] = vertex
 
-	def dfPaths(self,vertex):
-		self.dfs(vertex)
+	def dfPaths(self):
+		self._dfs(self._source)
+		self._trodden = True
 		return self._edge
 
+	def hasPathTo(self,vertex):
+		if not self._trodden:
+			print "Run DFS first. Tip: Instance Method dfPaths()."
+			return
+		return self._visited[vertex]
+
+	def pathTo(self,vertex):
+		if not self.hasPathTo(vertex):
+			return
+		node = vertex
+		path = Stack(vertex)
+		while node != self._source:
+			path.push(self._edge[node])
+			node = self._edge[node]
+		return path
+
+	def ppath(self,vertex):
+		st = self.pathTo(vertex)
+		if st is not None:
+			st.show()
+			return
+		print "No Path"
 
 if __name__ == '__main__':
 	graph = Graph(13)
@@ -82,15 +108,29 @@ if __name__ == '__main__':
 	graph.addEdge(12,9)
 	graph.addEdge(11,9)
 	graph.addEdge(11,12)
-	print "Adjacency Lists"
+	print "*"*40
+	print "{:^40}".format("Adjacency Lists")
+	print "*"*40
 	for item in range(13):
 		print graph.adjVertices(item)
+	print
+	print "*"*40
+	print "{:^40}".format("DFS Paths")
+	print "*"*40
+	dfs = DepthFirstPaths(graph,0)
+	print dfs.dfPaths()
+	print
+	print "*"*40
+	print "Path from source %s to each node." % (dfs._source)
+	print "*"*40
+	for item in range(13):
+		print "Path from 0 to",item,": ",
+		dfs.ppath(item)
+	print
 
-	dfs = DepthFirstPaths(graph)
-	print dfs.dfPaths(0)
-
-# Possible result
-# Adjacency Lists
+# ****************************************
+#             Adjacency Lists             
+# ****************************************
 # [1, 5, 2, 6]
 # [0]
 # [0]
@@ -105,4 +145,24 @@ if __name__ == '__main__':
 # [9, 12]
 # [9, 11]
 # 
+# ****************************************
+#                DFS Paths                
+# ****************************************
 # [0, 0, 0, 5, 3, 0, 4, 7, 8, 9, 10, 11, 12]
+# 
+# ****************************************
+# Path from source 0 to each node.
+# ****************************************
+# Path from 0 to 0 :  0
+# Path from 0 to 1 :  0 1
+# Path from 0 to 2 :  0 2
+# Path from 0 to 3 :  0 5 3
+# Path from 0 to 4 :  0 5 3 4
+# Path from 0 to 5 :  0 5
+# Path from 0 to 6 :  0 5 3 4 6
+# Path from 0 to 7 :  No Path
+# Path from 0 to 8 :  No Path
+# Path from 0 to 9 :  No Path
+# Path from 0 to 10 :  No Path
+# Path from 0 to 11 :  No Path
+# Path from 0 to 12 :  No Path# 
--- WeekX/Stack.py
+++ WeekX/Stack.py
@@ -3,7 +3,12 @@ from Node import Node
 class Stack(object):
 
 	def __init__(self,val):
+		if val == None:
+			self._first = None
+			self._size = 0
+			return
 		self._first = Node(val,None)
+		self._size = 1
 
 	def isEmpty(self):
 		return self._first == None
@@ -11,25 +16,32 @@ class Stack(object):
 	def push(self,val):
 		latest = Node(val,self._first)
 		self._first = latest
+		self._size += 1
 
 	def pop(self):
 		if not self.isEmpty():
 			popped = self._first
 			self._first = self._first._next
+			self._size -= 1
 			return popped
 		return
 
 	def show(self):
+		if self._size == 0:
+			return None
 		n = self._first
-		while n:
+		arr = [None]*self._size
+		for i in range(self._size):
 			print n._val,
+			arr[i] = n._val
 			n = n._next
 		print
+		return arr
 
 
 if __name__ == '__main__':
-	s = Stack('ketan')
-	text = "to be or not to - be - - that - - - is"
+	s = Stack(None)
+	text = "ketan to be or not to - be - - that - - - is"
 	for item in text.split():
 		if item == '-':
 			s.pop()
@@ -37,6 +49,7 @@ if __name__ == '__main__':
 			s.push(item)
 		s.show()
 
+# ketan
 # to ketan
 # be to ketan
 # or be to ketan
@@ -51,3 +64,4 @@ if __name__ == '__main__':
 # be to ketan
 # to ketan
 # is to ketan
+
